2724baea6e151e41e97974a04d9273a34ce20de5
Ruannino <105193525+ruannino@users.noreply.github.com>
2022-08-04 16:40:27 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import padrões.*;
+import padroes.*;
 
 public class Main {
     public static void main(String[] args) {
 
-       ProfissionalComissão representante = new ProfissionalComissão(
+       ProfissionalComissao representante = new ProfissionalComissao(
                 "Giovanna",
                 "22/07/2018",
                 "098.250.261-53",
@@ -14,7 +14,7 @@ public class Main {
         System.out.println(representante);
         System.out.println(representante.calculaRemuneracao(0.5));
 
-        ProfissionalSalário gerente = new ProfissionalSalário(
+        ProfissionalSalario gerente = new ProfissionalSalario(
                 "Ruan",
                 "22/03/1991",
                 "069.580.581-25",
